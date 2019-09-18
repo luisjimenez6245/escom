fd7bbcafce7188b502a8815729088c44a8b49c36
Luis <luisjimenez6245@hotmail.com>
2019-09-17 20:32:07 -0500

    :(

--- base/practicas/practica1/script.sql
+++ base/practicas/practica1/script.sql
@@ -52,64 +52,96 @@ insert into Asistencia values ('P3', 'A3', 'C1');
 insert into Asistencia values ('P5', 'A5', 'C2');
 insert into Asistencia values ('P5', 'A5', 'C1');
 
+Select * from Asistencia;
+Show CREATE TABLE Asistencia;
 alter table Asistencia add TempIdP char(3) null;
+Show CREATE TABLE Asistencia;
 
 update Asistencia set TempIdP = 'P3' where IdP = 'P1' and IdA = 'A1' and IdC = 'C1';
+Select * from Asistencia;
 update Asistencia set TempIdP = 'P3' where IdP = 'P1' and IdA = 'A2' and IdC = 'C3';
+Select * from Asistencia;
 update Asistencia set TempIdP = 'P3' where IdP = 'P2' and IdA = 'A4' and IdC = 'C1';
+Select * from Asistencia;
 update Asistencia set TempIdP = 'P5' where IdP = 'P3' and IdA = 'A3' and IdC = 'C3';
+Select * from Asistencia;
 update Asistencia set TempIdP = 'P5' where IdP = 'P3' and IdA = 'A3' and IdC = 'C2';
+Select * from Asistencia;
 update Asistencia set TempIdP = 'P1' where IdP = 'P4' and IdA = 'A2' and IdC = 'C1';
+Select * from Asistencia;
 update Asistencia set TempIdP = 'P1' where IdP = 'P4' and IdA = 'A2' and IdC = 'C2';
+Select * from Asistencia;
 update Asistencia set TempIdP = 'P1' where IdP = 'P3' and IdA = 'A3' and IdC = 'C1';
+Select * from Asistencia;
 update Asistencia set TempIdP = 'P1' where IdP = 'P5' and IdA = 'A5' and IdC = 'C2';
+Select * from Asistencia;
 update Asistencia set TempIdP = 'P5' where IdP = 'P5' and IdA = 'A5' and IdC = 'C1';
+Select * from Asistencia;
 
 alter table Asistencia add constraint FKIdProfesor FOREIGN KEY (IdP) REFERENCES Profesor(IdP);
+Show CREATE TABLE Asistencia;
 
 Delete From Asistencia where IdP = 'P2';
+Select * from Asistencia;
 Delete from Profesor where IdP = 'P2';
+Select * from Asistencia;
 
 alter table Asistencia drop FOREIGN KEY FKIdProfesor;
+Show CREATE TABLE Asistencia;
 
 alter table Asistencia add constraint FKIdProfesor FOREIGN KEY (IdP) REFERENCES Profesor(IdP) on Delete cascade on update cascade;
+Show CREATE TABLE Asistencia;
 
 update Profesor set idP ='P11' where idP = 'P4'; 
+Select * from Profesor;
 
 alter table Asistencia drop FOREIGN KEY FKIdProfesor;
-
 alter table Asistencia add constraint FKIdProfesor FOREIGN KEY (TempIdP) REFERENCES Profesor(IdP) on Delete set null on update set null;
+Show CREATE TABLE Asistencia;
 
 update Profesor set idP ='P8' where idP = 'P3'; 
+Select * from Profesor;
 
 alter table Asistencia drop FOREIGN KEY FKIdProfesor;
 alter table Asistencia add constraint FKIdProfesor FOREIGN KEY (TempIdP) REFERENCES Profesor(IdP) on Delete no action on update no action;
+Show CREATE TABLE Asistencia;
 
 update Profesor set idP ='P6' where idP = 'P1'; 
+Select * from Profesor;
 
 alter table Asistencia drop FOREIGN KEY FKIdProfesor;
 alter table Asistencia add constraint FKIdProfesor FOREIGN KEY (TempIdP) REFERENCES Profesor(IdP) on Delete restrict on update restrict;
+Show CREATE TABLE Asistencia;
 
 update Profesor set idP ='P7' where idP = 'P5'; 
+Select * from Profesor;
 
 alter table Asistencia drop FOREIGN KEY FKIdProfesor;
+Show CREATE TABLE Asistencia;
 
 update Asistencia set IdP = 'P8' where IdP = 'P3';
+Select * from Asistencia;
 
 
 alter table Asistencia add constraint FKIdProfesor FOREIGN KEY (IdP) REFERENCES Profesor(IdP) on Delete cascade on update cascade;
 alter table Asistencia add constraint FKIdClase FOREIGN KEY (IdC) REFERENCES Clase(IdC) on Delete cascade on update cascade;
 alter table Asistencia add constraint FKIdAsignatura FOREIGN KEY (IdA) REFERENCES Asignatura(IdA) on Delete cascade on update cascade;
+Show CREATE TABLE Asistencia;
 
 
 select timestampdiff(year, FechaNacimiento, now()) as Edad from profesor;
 
 alter TABLE  Profesor add Edad int; 
+Describe profesor;
 
 update Profesor set edad =  timestampdiff(year, FechaNacimiento, now());
+SELECT * From Profesor;
 
 alter table profesor add CONSTRAINT  ChEdad check(edad <= 40);
+Show CREATE TABLE profesor;
 
 insert into Profesor  values ('P15', 'Laura', 110, '1970/12/12', 48);
+SELECT * From Profesor;
 
 alter TABLE  Profesor add Edad2 int null check (edad2 <= 50); 
+Show CREATE TABLE Asistencia;
