f6ddcb619a7995275bcaa5cd9ded184f382aedcf
Luis <luisjimenez6245@hotmail.com>
2019-09-19 09:27:45 -0500

    :0

---
+++ base/practicas/practica3/script.sql
@@ -0,0 +1,117 @@
+SELECT NOW();
+SELECT SYSDATE();
+SELECT CURDATE();
+SELECT CURRENT_DATE();
+SELECT CURTIME();
+SELECT CURRRENT_TIME();
+SELECT YEAR("A");
+SELECT MONTH("A");
+SELECT MONTHNAME("A");
+SELECT QUATER("A");
+SELECT HOUR(12);
+SELECT MINUTE(12);
+SELECT SECOND(12);
+SELECT DAYNAME("A");
+SELECT DAYOFMONTH("A");
+SELECT DAYOFYEAR("A");
+SELECT DAYOFWEAK("A");
+
+
+SELECT ASCII("B");
+SELECT ASCII("B");
+
+SELECT CHAR(N);
+SELECT CHAR(N);
+
+SELECT CHAR(N, "AS");
+SELECT CHAR(N, "AS");
+
+SELECT BIN(100);
+SELECT BIN(20);
+
+SELECT CONCAT("", "");
+SELECT CONCAT("", "");
+
+SELECT CONCAT_WS('', "", "");
+SELECT CONCAT_WS('', "", "");
+
+SELECT HEX(200);
+SELECT HEX(220);
+
+SELECT OCT(40);
+SELECT OCT(100);
+
+SELECT LOWER("");
+SELECT LOWER("B");
+
+SELECT LCASE("B");
+SELECT LCASE("B");
+
+
+SELECT UPPER("Aquellos usureros sangraban con sus réditos a sus clientes.");
+SELECT UPPER("sangre: antiguamente se sangraba a los enfermos como método curativo.");
+
+SELECT UCASE("B");
+SELECT UCASE("");
+
+
+SELECT LEFT("A sangría que supone la emigración de cerebros afecta a toda América Latina.", 5);
+SELECT LEFT("Es un restaurante español en el que no faltan paella y sangría en el menú.", 6);
+
+SELECT RIGHT("A mí me gustan los sándwiches de jamón y queso.", 10);
+SELECT RIGHT(" comer siempre fuera de casa resulta una sangría para mí.", 4);
+
+SELECT LENGTH("Panguaracutimicuaro");
+SELECT LENGTH("esternocleidomastoideo");
+
+SELECT LTRIM("                    Guiso hecho con maíz");
+SELECT LTRIM("                    Barro muy espeso.");
+
+SELECT RTRIM("¡adios, qué tortazo!                 ", N);
+SELECT RTRIM("nos comprometemos en la defensa y desarrollo de nuestro estatuto     ", N);
+
+SELECT TRIM("      Anastacio es un perro.     ");
+SELECT TRIM("      Es imposible que un escritor no se comprometa     ");
+
+SELECT TRIM(Trailing '$' from "$Pretende que acabemos el trabajo antes del fin de semana$");
+SELECT TRIM(Trailing '$' from "$Dicho o hecho necio.$");
+
+SELECT TRIM(Leading '$' from "$El cuadro muestra grupos de jinetes$");
+SELECT TRIM(Leading '$' from "$Es dificil correr$");
+
+SELECT TRIM(Both '$' from "$El parque es seguro de noche.$");
+SELECT TRIM(Both '$' from "$El perro es un labrador$");
+SELECT TRIM('$' from "$El queso es rico en minerales$");
+SELECT TRIM('$' from "$Soy intolerante a la lactosa$");
+
+SELECT REPEAT("queso", 10);
+SELECT REPEAT("pan", 5);
+
+SELECT REVERSE("hipermetría");
+SELECT REVERSE("fallas");
+
+SELECT SUBSTRING("cabalgamiento", 4);
+SELECT SUBSTRING("Desplazamiento", 2);
+
+
+
+SELECT SUBSTRING("acebolladura", 8, 10);
+SELECT SUBSTRING("hipoalergénico", 3, 10);
+
+SELECT SUBSTR("Panguaracutimicuaro", 1, 3);
+SELECT SUBSTR("esternocleidomastoideo", 10, 5);
+
+
+SELECT SPACE(20);
+SELECT SPACE(15);
+
+SELECT LOCATE("perro", "Luis sacó al perro");
+SELECT LOCATE("Luis", "A Luis le gusta el pan");
+
+SELECT INSERT( "A Luis le gusta el pan", 10, 1, " queso y el ");
+SELECT INSERT( "10 es 11", 6, 1, " menor que ");
+
+SELECT REPLACE("A Luis%le% el pan",'%', ' ');
+SELECT REPLACE("DAILY&&%08:21:00 AM&&%19-09-2019&&%false&&%Lavar los dientes&&%",'&&%', ' ');
+
+
--- poo/practica2/executor.java
+++ poo/practica2/executor.java
@@ -1,28 +1,28 @@
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import java.awt.event.MouseEvent;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import javax.swing.JScrollPane;
 import javax.swing.SwingConstants;
+import javax.swing.event.MouseInputAdapter;
+
 
 public class executor {
 
@@ -33,8 +33,20 @@ public class executor {
 }
 
 class Remainder {
+
+    public boolean shouldRepeat;
+    public String hour;
+    public String day;
+    public Repetable isRepatable;
+    public String content;
+    public State state;
+
     public enum Repetable {
         DAILY, WEEKLY, MONTHLY, YEARLY,
+    }
+
+    public enum State {
+        COMPLETED, TOCOMPLETE, LATE,
     }
 
     public Remainder() {
@@ -67,16 +79,12 @@ class Remainder {
         return null;
     }
 
-    public boolean shouldRepeat;
-    public String hour;
-    public String day;
-    public Repetable isRepatable;
-    public String content;
 }
 
-class Panel extends Controller implements ActionListener {
+class Panel extends Controller {
 
     private static final String FILE = "/Users/luis/Documents/GitHub/escom/poo/practica2/remainder.txt";
+    private static final SimpleDateFormat TODATE = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss a");
     private static final long serialVersionUID = 1L;
     private JLabel clockLabel;
     private JLabel dateLabel;
@@ -93,25 +101,22 @@ class Panel extends Controller implements ActionListener {
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-    }
-
-    @Override
     protected void loadView() {
         JPanel panelClock = new JPanel();
         hourFormat = new SimpleDateFormat("hh:mm:ss a");
-        dateFormat = new SimpleDateFormat("DD-MM-YYYY");
+        dateFormat = new SimpleDateFormat("dd-MM-yyyy");
         setTitle("Reloj");
         setVisible(true);
         setLayout(null);
         setSize(320, 320);
         panelClock = new JPanel();
         panelRemainder = new JPanel();
-        dateLabel =  new JLabel("");
+        dateLabel = new JLabel("");
         clockLabel = new JLabel("");
         panelClock.setBounds(0, 20, 320, 100);
         panelRemainder.setLocation(0, 0);
-        JScrollPane scroll = new JScrollPane(panelRemainder, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        JScrollPane scroll = new JScrollPane(panelRemainder, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         scroll.setBounds(0, 100, 320, 220);
         panelRemainder.setLayout(new BoxLayout(panelRemainder, BoxLayout.Y_AXIS));
         clockLabel.setSize(270, 8);
@@ -119,12 +124,11 @@ class Panel extends Controller implements ActionListener {
         int x = (panelClock.getWidth() / 2) - (clockLabel.getWidth() / 2);
         int y = (panelClock.getHeight() / 2) - (clockLabel.getHeight() / 2);
         clockLabel.setLocation(x, y);
-        dateLabel.setLocation(x, y +100);
+        dateLabel.setLocation(x, y + 100);
         add(scroll);
         add(panelClock);
         panelClock.add(clockLabel);
         panelClock.add(dateLabel);
-
     }
 
     @Override
@@ -133,33 +137,96 @@ class Panel extends Controller implements ActionListener {
         clockLabel.setFont(new Font("Arial", Font.PLAIN, 35));
         dateLabel.setFont(new Font("Arial", Font.PLAIN, 5));
         remainders = new Remainder().build(Files.getFile(FILE));
+    }
+
+    @Override
+    protected void loadActions() {
         remiandersLabels = new ArrayList<>();
+        load(new Date());
+    }
+
+    private void load(Date date) {
+        List<Remainder> toReplace = new ArrayList<>();
         for (Remainder r : remainders) {
-            String toEval =  r.day + r.hour;
-            System.out.println(toEval);
-            JLabel l = new JLabel(r.content, SwingConstants.CENTER);
+            toReplace.add(getState(date, r));
+        }
+        this.remainders = toReplace.toArray(new Remainder[toReplace.size()]);
+        Font f =   new Font("Arial", Font.PLAIN, 25);
+        for (Remainder r : remainders) {
+            JLabel l;
+            if (r.state.equals(Remainder.State.COMPLETED))
+                l = new JLabel(r.content, SwingConstants.CENTER);
+            else
+                l = new JLabel(r.content, SwingConstants.CENTER);
             l.setSize(320, 100);
+            l.setFont(f);
             l.setHorizontalAlignment(SwingConstants.CENTER);
             l.setVerticalAlignment(SwingConstants.CENTER);
+            l.addMouseListener(new MouseInputAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    JLabel source = (JLabel) e.getSource();
+                    int i = 0;
+                    for (i = 0; i < remiandersLabels.size(); ++i) {
+                        if (remiandersLabels.get(i).equals(source)) {
+                            Remainder r = remainders[i];
+                            JOptionPane.showMessageDialog(rootPane, r.content);
+                        }
+                    }
+                }
+            });
             remiandersLabels.add(l);
             panelRemainder.add(l);
         }
     }
 
-    @Override
-    protected void loadActions() {
+    private void changeHour(Date date) {
+        clockLabel.setText(hourFormat.format(date));
+        dateLabel.setText(dateFormat.format(date));
+        List<Remainder> toReplace = new ArrayList<>();
+        for (Remainder r : remainders) {
+            toReplace.add(getState(date, r));
+        }
+        this.remainders = toReplace.toArray(new Remainder[toReplace.size()]);
     }
 
-    private void changeHour(String date, String day) {
-        clockLabel.setText(date);
+    private Remainder getState(Date date, Remainder remainder) {
+        try {
+            Date dateRemainder = TODATE.parse(remainder.day + ' ' + remainder.hour);
+            if (date.toString().equals(dateRemainder.toString())) {
+                JOptionPane.showMessageDialog(rootPane, "Es hora de: " + remainder.content);
+             } else {
+                if (remainder.shouldRepeat) {
+                    dateRemainder = TODATE.parse(remainder.day + ' ' + remainder.hour);
+                    remainder.state = Remainder.State.COMPLETED;
+                    if (date.compareTo(dateRemainder) == 0) {
+
+                        JOptionPane.showMessageDialog(rootPane, "alv");
+                    }
+                } else {
+                    dateRemainder = TODATE.parse(remainder.day + ' ' + remainder.hour);
+                    if (date.compareTo(dateRemainder) > 0) {
+                        remainder.state = Remainder.State.LATE;
+                    } else {
+                        if (date.compareTo(dateRemainder) < 0) {
+                            remainder.state = Remainder.State.TOCOMPLETE;
+                        } else {
+                            System.out.println("kjsndjas");
+                        }
+                    }
+                }
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return remainder;
     }
 
     private void manageClock() {
         new Thread() {
             public void run() {
                 while (true) {
-                    Date date = new Date();
-                    changeHour(hourFormat.format(date), dateFormat.format(date) );
+                    changeHour(new Date());
                     try {
                         sleep(1000);
                     } catch (InterruptedException e) {
