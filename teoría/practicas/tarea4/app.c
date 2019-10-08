c448f6068aa1bc258bb0c231b6a40792264aa691
Luis <luisjimenez6245@hotmail.com>
2019-10-07 23:29:03 -0500

    :(

--- teoría/practicas/tarea4/app.c
+++ teoría/practicas/tarea4/app.c
@@ -10,52 +10,63 @@ typedef unsigned char boolean;
 typedef struct object
 {
     boolean shouldContinue;
-	boolean isValid;
-	int state;
+    boolean isValid;
+    int state;
 } object;
 
-void manageProcess(char * fileName);
+void manageProcess(char *fileName);
 object manageObject(object *obj);
 boolean isValidWord(char c, object *process0, object *process1);
 
 int main(int argc, const char **argv)
 {
-    char * fileName = "./file.txt";
-      if (argc == 2)
+    char *fileName = "./file.txt";
+    if (argc == 2)
     {
         fileName = argv[1];
     }
 }
 
-void manageProcess(char * fileName)
+void manageProcess(char *fileName, char *resFileName)
 {
     FILE *file;
+    FILE *res;
+    FILE *aux;
     file = fopen(fileName, "r");
     char toWork;
-    do 
+    do
     {
-      toWork = (char) fgetc(file);
-      if (isValidWord(toWork ,  &process0, &process1))
-      {
+        toWork = (char)fgetc(file);
+        if (isValidWord(toWork, &process0, &process1))
+        {
+        }
+        else
+        {
 
-      }
-    } while(toWork != EOF);
+        }
+    } while (toWork != EOF);
     return code;
 }
 
-boolean isValidWord(char c, object * process[], int process number)
+boolean isValidWord(char c, object *process[], int process number)
 {
-    
+    int i;
+    for (i = 0; i < number; ++i)
+    {
+        evalObject(process[i], c);
+    }
 }
 
 object evalObject(object *obj, char c)
 {
-    if (obj -> shouldContinue){
-        if(c == "0" && obj -> state == 1){
-
+    if (obj->shouldContinue)
+    {
+        if (c == "0" && obj->state == 1)
+        {
         }
     }
-    else{
+    else
+    {
 
     }
 }
