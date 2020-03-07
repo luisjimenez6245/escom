575dd3c329b491724ababe5e85dc717331d4a148
Luis Jiménez <luisjimenez6245@hotmail.com>
2020-03-06 20:11:24 -0600

    chale

--- so/practica4/ejerB.c
+++ so/practica4/ejerB.c
@@ -1,53 +1,57 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
+void create_process_child(char *command);
+char *getCommand();
 int call_proc_from_string(char *proc);
 int getArgCount(char *proc);
-/*
-int call_proc(char *name, char *args[]);
-*/
-int main()
+
+int main(int argc, const char **argv)
 {
-  char opc[] = "hellow";
+  create_process_child(getCommand());
+  return 0;
+}
+
+void create_process_child(char *command)
+{
   int pid, status;
-  while (strcmp(opc, "exit") != 0)
+  while (strcmp(command, "exit") != 0)
   {
     pid = fork();
     if (pid == -1)
     {
-      printf("Error en la llamada a fork\n");
-      exit(-1);
+      printf("Error en el fork");
+      exit(1);
     }
     else
     {
       if (pid == 0)
       {
-        printf("Introduce el comando>\t");
-        scanf("%[^\n]", opc);
-        if (strcmp(opc, "exit") != 0)
-        {
-          int res  = call_proc_from_string(opc);
-          printf("res %i:\n", res);
-          exit(res);
-        }
+        printf("\nHIJO Mi PID es: %i \n Mi padre es: %i\n", getpid(), getppid());
+        exit(call_proc_from_string(command));
       }
       else
       {
         pid = wait(&status);
+        if (WIFEXITED(status))
+        {
+          printf("PADRE \t Mi hijo con PID: %i termino con estado %i\n", pid, WEXITSTATUS(status));
+          command = (getCommand());
+        }
       }
     }
   }
-  return 0;
 }
 
-int call_proc_from_string(char *proc)
+int call_proc_from_string(char *command)
 {
   int count = 0;
-  char *args[getArgCount(proc) + 1];
-  char *token = strtok(proc, " ");
+  char *args[getArgCount(command) + 1];
+  char *token = strtok(command, " ");
   while (token != NULL)
   {
     args[count] = token;
@@ -55,11 +59,14 @@ int call_proc_from_string(char *proc)
     token = strtok(NULL, " ");
   }
   args[count] = (char *)0;
-  for(int i = 0; i <= count; ++i ){
+  for (int i = 0; i <= count; ++i)
+  {
     printf("%s\n", args[i]);
   }
-  int res = execvp("./comando.out", args);
-  printf("res %i:\n", res);
+  int res = 0;
+  //int res = execvp("./comando.out", args);
+  printf("res : %i\n", res);
+  fflush(stdout);
   return res;
 }
 
@@ -75,13 +82,14 @@ int getArgCount(char *proc)
   }
   return count;
 }
-/*
-int call_proc(char *name, char *args[])
+
+char *getCommand()
 {
-  int res = execvp(name, args);
-  if (res != 0)
-  {
-    printf("Ocurrió un error en la llamada\n");
-  }
-  return 0;
-}*/
+  char *command = malloc(100 * sizeof(char));
+  printf("Introduce el comando>\t");
+  scanf("%[^\n]", command);
+  fflush(stdin);
+  printf("%s\n", command);
+  fflush(stdin);
+  return command;
+}
