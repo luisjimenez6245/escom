66913e5ba8b76c0cb1fa827739c06b40209aac40
Luis Jiménez <luisjimenez6245@hotmail.com>
2020-05-27 11:28:23 -0500

    Update comando.c

--- so/proyecto/comando.c
+++ so/proyecto/comando.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #define INPUT_END 1
 #define OUTPUT_END 0
@@ -44,31 +47,89 @@ int has_pipe(char **args, int size)
     return 0;
 }
 
-char *get_command_from_pipe(char *command, int position)
+int get_arg_count(char *proc, char to_search)
 {
-    int i = 0;
-    char *res;
+  int count = 0;
+  for (int i = 0; i < strlen(proc); ++i)
+  {
+    if (proc[i] == to_search)
+    {
+      ++count;
+    }
+  }
+  return count;
+}
+
+char *get_command_from_pipe(char *command, int position, int complete_string)
+{
+    int i = 0, count = get_arg_count(command, '|');
     char *token = strtok(command, "|");
-    printf("token:%s \n", token);
     for (i = 0; i < position; ++i)
     {
         token = strtok(NULL, "|");
-        printf("token:%s \n", token);
     }
     if (token != NULL)
-        res = token;
+    {
+        if(complete_string)
+        {
+            char *res[count];
+            res[0] = token;
+            for (i = 1; i < count; ++i)
+            {
+                token = strtok(NULL, "|");
+                            res[i] = token;
+            }
+            return concatenate(count, res, "|");
+        }
+        else
+        {
+            return token;
+        }
+    }
     else
-        res = "";
-    return res;
+        return "";
 }
+
+int call_proc_from_string(char *command)
+{
+  printf("commando: %s\n", command);
+  int count = 0;
+  char *args[get_arg_count(command, ' ')];
+  char *token = strtok(command, " ");
+  while (token != NULL)
+  {
+    args[count] = token;
+    count += 1;
+    token = strtok(NULL, " ");
+  }
+  args[count] = (char *)0;
+  int res = execvp(args[0], args);
+  return res;
+}
+
+int call_proc_from_string_c(char *command)
+{
+  printf("commando: %s\n", command);
+  int count = 1;
+  char *args[get_arg_count(command, ' ') + 1];
+  char *token = strtok(command, " ");
+  args[0]  = "./comando.out";
+  while (token != NULL)
+  {
+    args[count] = token;
+    count += 1;
+    token = strtok(NULL, " ");
+  }
+  args[count] = (char *)0;
+  int res = execvp(args[0], args);
+  return res;
+}
+
 
 int manage_pipe(char *first_command, char *second_command)
 {
     pid_t pid;
     int fd[2];
-    char *argxs[3];
-    argxs[1] = "-l";
-    argxs[2] = (char *)0;
     pipe(fd);
     pid = fork();
 
@@ -77,8 +138,7 @@ int manage_pipe(char *first_command, char *second_command)
         dup2(fd[INPUT_END], STDOUT_FILENO);
         close(fd[OUTPUT_END]);
         close(fd[INPUT_END]);
-        argxs[0] = "ls";
-        execvp("ls", argxs);
+        call_proc_from_string(first_command);
         fprintf(stderr, "Failed to execute '%s'\n", "ls");
         exit(1);
     }
@@ -91,8 +151,7 @@ int manage_pipe(char *first_command, char *second_command)
             dup2(fd[OUTPUT_END], STDIN_FILENO);
             close(fd[INPUT_END]);
             close(fd[OUTPUT_END]);
-            argxs[0] = "wc";
-            execvp("wc", argxs);
+            call_proc_from_string_c(second_command);
             fprintf(stderr, "Failed to execute '%s'\n", "wc");
             exit(1);
         }
@@ -116,13 +175,12 @@ int main(int argc, const char **argv)
     }
     args[argc - 1] = (char *)0;
     char *command = concatenate(argc - 1, args, " ");
-    printf("comando %s\n", command);
     if (strchr(command, '|') != NULL)
     {
-        char *first_command = get_command_from_pipe(command, 0);
-        char *next_command = get_command_from_pipe(command, 1);
-        printf("f:%s\n", first_command);
-        printf("s:%s\n", next_command);
+        char * copy = malloc(strlen(command) + 1); 
+        char * first_command = get_command_from_pipe(strcpy(copy, command), 0, 0);
+        char * next_command = get_command_from_pipe(strcpy(copy, command), 1, 1);
+        return manage_pipe(first_command,next_command);
     }
     else
     {
