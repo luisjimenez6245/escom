e05b3d03ef39edf4bc4503ddeb246ff273102b50
Luis Jiménez <luisjimenez6245@hotmail.com>
2020-10-22 14:35:48 -0500

    adds all graphs

---
+++ analis de algoritmos/tarea1/graph.sh
@@ -0,0 +1,4 @@
+for ((VAR=1 ; VAR<=5 ; VAR++ ))
+do
+   python3 grapher.py "ejer$VAR"
+done
--- analis de algoritmos/tarea1/run.sh
+++ analis de algoritmos/tarea1/run.sh
@@ -1,25 +1,6 @@
-#!/bin/bash
 
-array[0]=-1
-array[1]=0
-array[2]=1
-array[3]=2
-array[4]=3
-array[5]=5
-array[6]=15
-array[7]=20
-array[8]=100
-array[9]=409
-array[10]=500
-array[11]=593
-array[12]=1000
-array[13]=1471
-array[14]=1500
-array[15]=2801
-array[16]=3000
-array[17]=5000
-array[18]=10000
-array[19]=20000
+array=(-1, 0, 1, 2, 3, 5, 15, 20, 100, 409, 500, 593,
+       1000, 1471, 1500, 2801, 3000, 5000, 10000, 20000)
 
 
 echo "" > "./$1/res.txt"
