592c11389a2c3d0529275e4b7816922204667eab
Luis Jiménez <luisjimenez6245@hotmail.com>
2021-03-25 22:34:56 -0600

    aaa

--- ddb/pl_sql_by_example/c7/ejercicios.sql
+++ ddb/pl_sql_by_example/c7/ejercicios.sql
@@ -1,4 +1,4 @@
-SPOOL C:\Users\luis\Documents\Github\ESCOM\ddb\pl_sql_by_example\c7\salida.txt
+SPOOL C:\Users\luisj\Documents\GitHub\escom\ddb\pl_sql_by_example\c7\salida.txt
 rem ROSENZWEIG, B & SILVESTROVA, E. (2009). Oracle® PL/SQL™ by Example,USA:Perarson.
 
 
--- ddb/sql_by_example/c3/ejercicios.sql
+++ ddb/sql_by_example/c3/ejercicios.sql
@@ -2,6 +2,10 @@ SPOOL C:\Users\luis\Documents\Github\ESCOM\ddb\sql_by_example\c3\salida.txt
 rem RISCHERT, A (2004).Oracle® SQL by Example,Upper Saddle River,NJ,USA:Perarson.
 
 
+SELECT student_id, last_name FROM student WHERE INSTR(last_name, 'o', 1, 3) > 0;
+
+
+
 
 
 SPOOL OFF
